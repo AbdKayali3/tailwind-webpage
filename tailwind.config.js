15f2149684dfa26ca2e11ee44f46202779566204
unknown <akayali532@gmail.com>
2023-05-11 19:22:36 +0200

    makign the header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#1E1E1E',
+        'secondary': '#2F2F2F',
+        'bgmain': '#222',
+        'bghelper': '#444',
+
+        'gradient1': '#d7ebeb',
+        'gradient2': '#f4afe9',
+        'gradient3': '#9d7ef3',
+      }
+    },
   },
   plugins: [],
 }
